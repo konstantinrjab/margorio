ba450f0d74d5ec05164d66d2b6618ae92961cfa6
konstantin <konstantinrjab@gmail.com>
2022-10-11 01:36:19 +0200

    didgitalocean

--- .deploy/deploy.php
+++ .deploy/deploy.php
@@ -8,17 +8,21 @@ echo "Start job...\n";
 echo "\nGit pull\n";
 exec('cd /var/www && git pull origin master 2>&1', $output);
 print_r($output);
+unset($output);
 
 echo "\nComposer install\n";
 exec('cd /var/www && composer install 2>&1', $output);
 print_r($output);
+unset($output);
 
 echo "\nMigrate\n";
-exec('php /var/www/artisan migrate --force > /dev/null 2>&1 &', $output);
+exec('php /var/www/artisan migrate --force 2>&1', $output);
 print_r($output);
+unset($output);
 
 echo "\nClear Cache\n";
-exec('php /var/www/artisan route:cache > /dev/null 2>&1 &', $output);
+exec('php /var/www/artisan route:cache 2>&1', $output);
 print_r($output);
+unset($output);
 
 echo '</pre>';
