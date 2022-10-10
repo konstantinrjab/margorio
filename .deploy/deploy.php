8035300ca556818880ce511a475d91666fd4e190
konstantin <konstantinrjab@gmail.com>
2022-10-11 00:54:53 +0200

    didgitalocean

--- .deploy/deploy.php
+++ .deploy/deploy.php
@@ -2,8 +2,15 @@
 
 // TODO: check header: X-Hub-Signature
 
-exec('cd ../ && git pull origin master');
-exec('cd ../ && composer install');
-exec('php ../artisan migrate --force');
-exec('php ../artisan route:cache');
+exec('cd ../ && git pull origin master', $output);
+print_r($output);
+
+exec('cd ../ && composer install', $output);
+print_r($output);
+
+exec('php ../artisan migrate --force', $output);
+print_r($output);
+
+exec('php ../artisan route:cache', $output);
+print_r($output);
 
